9a57caa4105c3303d5776206235a10f76973998d
es0612 <hyai0323@gmail.com>
2020-08-18 10:23:17 +0900

    update background task

--- app/LeafTimer/App/AppDelegate.swift
+++ app/LeafTimer/App/AppDelegate.swift
@@ -5,7 +5,12 @@ import SwiftUI
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+
     var backgroundTaskID: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0)
+
+    var oldBackgroundTaskID: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0)
+    var timer: Timer?
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
@@ -35,14 +40,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 新しいタスクを登録
         backgroundTaskID = application.beginBackgroundTask {
             [weak self] in
+            
             application.endBackgroundTask((self?.backgroundTaskID)!)
             self?.backgroundTaskID = UIBackgroundTaskIdentifier.invalid
         }
+
+        // タスクが止まらないようにアップデート
+        timer = Timer.scheduledTimer(
+            withTimeInterval: 30,
+            repeats: true,
+            block: { _ in
+                self.oldBackgroundTaskID = self.backgroundTaskID
+
+                // 新しいタスクを登録
+                self.backgroundTaskID = application.beginBackgroundTask() {
+                    [weak self] in
+
+                    application.endBackgroundTask((self?.backgroundTaskID)!)
+                    self?.backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+                }
+                // 前のタスクを削除
+                application.endBackgroundTask(self.oldBackgroundTaskID)
+        }
+        )
     }
     
     //アプリがアクティブになる度に呼ばれる
     func applicationDidBecomeActive(_ application: UIApplication) {
         //タスクの解除
+        timer?.invalidate()
         application.endBackgroundTask(self.backgroundTaskID)
     }
 
