d1d8b10433d3eaa567ac21c3227ddb0f92e1d697
es0612 <hyai0323@gmail.com>
2020-08-18 09:39:24 +0900

    processing on background

--- app/LeafTimer/App/AppDelegate.swift
+++ app/LeafTimer/App/AppDelegate.swift
@@ -1,29 +1,50 @@
 import UIKit
+import SwiftUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
+    var backgroundTaskID: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+
+        let contentView = TimerView(
+            timverViewModel: TimerViewModel(
+                timerManager: DefaultTimerManager(),
+                audioManager: DefaultAudioManager(),
+                userDefaultWrapper: LocalUserDefaultsWrapper()
+            ),
+            settingViewModel: SettingViewModel(
+                userDefaultWrapper: LocalUserDefaultsWrapper()
+            )
+        )
+
+        let vc = UIHostingController(rootView: contentView)
+
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    //バックグラウンド遷移移行直前に呼ばれる
+    func applicationWillResignActive(_ application: UIApplication) {
+        // 新しいタスクを登録
+        backgroundTaskID = application.beginBackgroundTask {
+            [weak self] in
+            application.endBackgroundTask((self?.backgroundTaskID)!)
+            self?.backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    //アプリがアクティブになる度に呼ばれる
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        //タスクの解除
+        application.endBackgroundTask(self.backgroundTaskID)
     }
 
 
 }
-
